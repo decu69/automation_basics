a92f74412eddbc157238eff1ae0c409f1072009d
Daniel Dec <ddec@future-processing.com>
2017-06-21 00:56:59 +0200

    ToDos

--- pages/sendBugPage.py
+++ pages/sendBugPage.py
@@ -4,26 +4,23 @@ class SendBugPage(BasePage):
     _validationText = "Type your team id and bug content:"
 
     _teamIdEditBox = "/html/body/div/div/form[1]/input"
-    _bugDescription = "/html/body/div/div/form[1]/textarea"
-    _sendBugButton = "//*[@id='send-button']"
+    #toDo
 
     _team_error_message = "/html/body/div/div/h1"
 
     def enter_team_id(self, teamid):
         inputbox = self.driver.find_element_by_xpath(self._teamIdEditBox)
         inputbox.clear()
-        inputbox.send_keys(teamid)
+        #toDo
 
     def enter_bug_description(self, text):
-        inputbox = self.driver.find_element_by_xpath(self._bugDescription)
-        inputbox.clear()
-        inputbox.send_keys(text)
+        #ToDo
 
     def sendBug(self):
-        self.driver.find_element_by_xpath(self._sendBugButton).click()
+        #ToDo
 
     def getSendingBugStatus(self):
-        return self.driver.find_element_by_xpath(self._team_error_message).text
+        #ToDo
 
 
     def _validate_page(self):
--- tests/test_adding_bugs.py
+++ tests/test_adding_bugs.py
@@ -8,11 +8,9 @@ class AddingBugInSystemTest(BaseTestCase):
 
     def testUnauthorizedTeamCouldNotAddBug(self):
         sendBugPage = MainPage().sendBug()
-        sendBugPage.enter_team_id("123")
-        sendBugPage.enter_bug_description("testBug")
-        sendBugPage.sendBug()
+        #toDo
 
-        assert sendBugPage.getSendingBugStatus() == u"Team with id: '123' was not found."
+        assert sendBugPage.getSendingBugStatus() == #ToDO
 
 if __name__ == '__main__':
     unittest.main()
